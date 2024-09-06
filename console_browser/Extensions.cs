e3bf26ef1a05e0c036a4f265041522395133ffcf
Dr. A <4411916+scotpatti@users.noreply.github.com>
2024-09-06 12:32:31 -0400

    Add project files.

---
+++ console_browser/Extensions.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace console_browser
+{
+    public static class Extensions
+    {
+        public static bool WriteString(this NetworkStream stream, string data)
+        {
+            bool rval = true;
+            try
+            {
+                byte[] buffer = ASCIIEncoding.ASCII.GetBytes(data);
+                stream.Write(buffer, 0, buffer.Length);
+                stream.Flush();
+            }
+            catch
+            {
+                rval = false;
+            }
+            return rval;
+        }
+
+        public static string ReadString(this NetworkStream stream)
+        {
+            StringBuilder sb = new StringBuilder();
+            byte[] buffer = new byte[65536];
+            stream.ReadTimeout = 1000;
+            try
+            {
+                do
+                {
+                    stream.Read(buffer, 0, buffer.Length);
+                    sb.Append(ASCIIEncoding.ASCII.GetString(buffer));
+                } while (stream.DataAvailable);
+            }
+            catch (Exception)
+            {
+                throw new Exception("Connection timed out.");
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ console_browser/Program.cs
@@ -0,0 +1,21 @@
+﻿using console_browser;
+using System;
+using System.Net.Sockets;
+using System.Text;
+
+string hostname = "httpforever.com";
+string request = "GET / HTTP/1.0\r\n\r\n";
+//byte[] buffer = ASCIIEncoding.ASCII.GetBytes(request);
+
+TcpClient client = new TcpClient();
+client.Connect(hostname, 80);
+
+NetworkStream stream = client.GetStream();
+
+stream.WriteString(request);
+string data = stream.ReadString();
+
+stream.Close();
+client.Close();
+
+Console.WriteLine(data);
